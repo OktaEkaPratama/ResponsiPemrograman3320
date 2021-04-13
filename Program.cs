45fb499f0e28460ead2e9f8fbf27bd9cd0885a9a
OktaEkaPratama <oktapratama@students.amikom.ac.id>
2021-04-13 12:37:35 +0700

    responsipemrograman3320

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ResponsiPemrograman3320
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nama     : Okta Eka Pratama");
+            Console.WriteLine("NIM      : 20.11.3320");
+            Console.WriteLine("Kelas    : IF 01");
+
+            Console.WriteLine("\n");
+
+
+            Karyawan karyawan1 = new Karyawan(1911, "Joko", 4000000);
+            Karyawan karyawan2 = new Karyawan(2742, "Slamet", 3000000);
+            Karyawan karyawan3 = new Karyawan(1519, "Jaya", -200000);
+
+
+            Console.WriteLine("==================================================");
+            Console.WriteLine(" NO | NIK / NAMA\t\t | Gaji Bulanan\t |");
+            Console.WriteLine("==================================================");
+            Console.WriteLine(" 1. | {0} {1} \t\t | {2}\t |", karyawan1.NIK, karyawan1.Nama, karyawan1.GajiBulanan);
+            Console.WriteLine(" 2. | {0} {1} \t\t | {2}\t |", karyawan2.NIK, karyawan2.Nama, karyawan2.GajiBulanan);
+            Console.WriteLine(" 3. | {0} {1} \t\t | {2}\t\t |", karyawan3.NIK, karyawan3.Nama, karyawan3.GajiBulanan);
+
+            Console.WriteLine("\n\nAsikkkkk dpt kenaikan gaji nih!!!\n");
+
+            Console.WriteLine("==================================================");
+            Console.WriteLine(" NO | NIK / NAMA\t\t | Gaji Bulanan\t |");
+            Console.WriteLine("==================================================");
+            Console.WriteLine(" 1. | {0} {1} \t\t | {2}\t |", karyawan1.NIK, karyawan1.Nama, karyawan1.NaikGaji);
+            Console.WriteLine(" 2. | {0} {1} \t\t | {2}\t |", karyawan2.NIK, karyawan2.Nama, karyawan2.NaikGaji);
+            Console.WriteLine(" 3. | {0} {1} \t\t | {2}\t\t |", karyawan3.NIK, karyawan3.Nama, karyawan3.GajiBulanan);
+
+
+            Console.ReadKey();
+        }
+    }
+
+}
